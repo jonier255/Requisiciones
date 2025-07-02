8deec973f8f441d4ae8eda38ad4a9972ff98dd9e
Sebastian Hernadez <sebastian.hernandez.scarpetta@correounivalle.edu.co>
2025-07-01 21:16:41 -0500

    refactor(database): remove debug log for DATABASE_URL

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('database_url:', process.env.DATABASE_URL);
-
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
